ef8b3dbacd3dffa9cce8173bb0ff1b9ba8ff94a2
kaushik-regeti-07 <137099391+kaushik-regeti-07@users.noreply.github.com>
2026-09-02 14:58:00 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        // if(s.length() != t.length()) {
+        //     return false;
+        // } 
+
+        // HashMap<Character, Integer> map = new HashMap<>();
+
+        // for(char c:s.toCharArray()){
+        //     map.put(c,map.getOrDefault(c,0)+1);
+        // }
+
+        // for(char c:t.toCharArray()){
+        //     if(!map.containsKey(c)){
+        //         return false;
+        //     }
+
+        //     int count = map.get(c);
+        //     if(count == 1){
+        //         map.remove(c);
+        //     }else{
+        //         map.put(c,count-1);
+        //     }
+        // }
+        // return map.isEmpty();
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        int[] count = new int[26];
+
+        for(int i=0; i<s.length(); i++){
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for(int c:count){
+            if(c!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
